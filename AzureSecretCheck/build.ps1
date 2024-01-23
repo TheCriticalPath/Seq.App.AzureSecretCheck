4e967375d30b5866ff3b4df75923ef99e4374eb8
Tony Clark <tony.j.clark@gmail.com>
2024-01-22 20:28:56 -0500

    iterative fix

--- AzureSecretCheck/build.ps1
+++ AzureSecretCheck/build.ps1
@@ -18,7 +18,7 @@ $suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch
 
 echo "build: Version suffix is $suffix"
 
-foreach ($src in ls src/*) {
+foreach ($src in ls AzureSecretCheck/*) {
     Push-Location $src
 
     echo "build: Packaging project in $src"
